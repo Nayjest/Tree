e5e764f58d097eb14ab506defbf9b2abe607a82b
v.stepanenko <v.stepanenko@globo-tech.com>
2016-01-20 16:26:31 +0200

    NodeConstructorTrait removed

--- src/Node.php
+++ src/Node.php
@@ -4,6 +4,19 @@ namespace Nayjest\Tree;
 
 class Node implements NodeInterface
 {
-    use NodeConstructorTrait;
     use NodeTrait;
+
+    /**
+     * Node constructor.
+     * @param ParentNodeInterface|null $parent
+     * @param array|null $children
+     */
+    public function __construct(
+        ParentNodeInterface $parent = null,
+        array $children = null
+    )
+    {
+        ($children !== null) && $this->initializeCollection($children);
+        $parent && $parent->children()->add($this);
+    }
 }
