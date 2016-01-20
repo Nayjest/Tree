f44161fcd76b7036d12760fa72466c1c4db6bf59
v.stepanenko <v.stepanenko@globo-tech.com>
2016-01-20 16:31:28 +0200

    ReadonlyParentNodeTrait removed

--- src/ReadonlyNodeTrait.php
+++ src/ReadonlyNodeTrait.php
@@ -2,8 +2,30 @@
 
 namespace Nayjest\Tree;
 
+use Nayjest\Collection\Decorator\ReadonlyObjectCollection;
+
 trait ReadonlyNodeTrait
 {
     use ChildNodeTrait;
-    use ReadonlyParentNodeTrait;
+    use ParentNodeTrait {
+        ParentNodeTrait::children as protected writableChildren;
+    }
+
+    private $readonlyCollection;
+
+    /**
+     * Returns child components.
+     *
+     * @return ReadonlyObjectCollection
+     */
+    public function children()
+    {
+        if ($this->readonlyCollection === null) {
+            $this->readonlyCollection = new ReadonlyObjectCollection(
+                $this->writableChildren()
+            );
+        }
+
+        return $this->readonlyCollection;
+    }
 }
